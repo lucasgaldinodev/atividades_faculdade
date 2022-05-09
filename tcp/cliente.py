efb38b03d3a94beebcc6391893f85530b9980b03
Lucas Galdino <lucasgaldino7@gmail.com>
2022-05-09 20:31:15 -0300

    tcp chat

--- tcp/cliente.py
+++ tcp/cliente.py
@@ -1,9 +1,28 @@
 import socket
+import threading
 
+threads = list()
 cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-cliente.connect(('0.0.0.0', 8080))
+cliente.connect(('0.0.0.0', 8081))
 
-while True:
-    cliente.send(input("Digite a mensagem: ").encode())
-    mensagem = cliente.recv(1024)
-    print(mensagem.decode("utf-8"))
+
+def enviar(cliente):
+    while True:
+        cliente.send(input("Digite a mensagem: \n").encode())
+
+
+def receber(cliente):
+    while True:
+        mensagem = cliente.recv(1024)
+        print(mensagem.decode("utf-8"))
+        print('-----')
+
+
+enviarTh = threading.Thread(target=enviar, args=(cliente,))
+receberTh = threading.Thread(target=receber, args=(cliente,))
+
+receberTh.start()
+enviarTh.start()
+
+# for index, thread in enumerate(threads):
+#     thread.start()
--- tcp/servidor.py
+++ tcp/servidor.py
@@ -1,16 +1,27 @@
 import socket
+import threading
 
 servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-servidor.bind(('0.0.0.0', 8080))
+servidor.bind(('0.0.0.0', 8081))
 servidor.listen()
+
+listaClientes = []
+
+
+def receber(cliente):
+    while True:
+        print(f"Recebeu conexão de {endCliente}")
+        mensagem = cliente.recv(1024)
+        for c in listaClientes:
+            # if c == cliente:
+            #     mensagem = cliente.recv(1024)
+            #     print(f'{endCliente}: {mensagem.decode("utf-8")}')
+            print(f'{endCliente}: {mensagem.decode("utf-8")}')
+            c.send(mensagem)
+
 
 while True:
     cliente, endCliente = servidor.accept()
-    print(f"Recebeu conexão de {endCliente}")
-    while True:
-        # try:
-        mensagem = cliente.recv(1024)
-        print(mensagem.decode("utf-8"))
-        cliente.send(mensagem)
-        # except:
-        #     break
+    listaClientes.append(cliente)
+    enviarThServer = threading.Thread(target=receber, args=(cliente,))
+    enviarThServer.start()
